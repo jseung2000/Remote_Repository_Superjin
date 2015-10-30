e2605f344e464f34153ccc9e2b4faf8647e49844
jseung <jin.seung@ericsson.com>
2015-10-30 14:15:09 +0900

    Hello.cpp 추가

---
+++ Hello.cpp
@@ -0,0 +1,26 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: 2015. 10. 30.
+ *      Author: ejinseu
+ */
+
+//============================================================================
+// Name        : Hello.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Jin!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
+
